8d70e4092c3ed8d068a7eb8ae6ea54f34f60dd1e
DGolyshkin <eremarambler@list.ru>
2023-11-07 15:10:22 +0300

    git-rebase 2

--- 02-git-03-branching/rebase.sh
+++ 02-git-03-branching/rebase.sh
@@ -3,11 +3,15 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
 HEAD
     echo "\$@ Parameter #$count = $param"
 
     echo "Parameter: $param"
 261973b (git-rebase 1)
+=======
+    echo "Next parameter: $param"
+>>>>>>> e6b74b6 (git-rebase 2)
     count=$(( $count + 1 ))
 done
 
